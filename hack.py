9b883d4a435e76e0c70a0299621d6fbac0d2b6e4
KYLE SCHILLER <schillerk@carleton.edu>
2017-04-14 20:17:42 -0500

    added api call for keywords

--- hack.py
+++ hack.py
@@ -1,9 +1,9 @@
 from lxml import html
 import requests
-
-page = requests.get('https://www.soylent.com/')
-print page.content
-
 import indico
 
 indicoio.config.api_key = '246290703649a7500961ea78369dbce8'
+
+page = requests.get('https://www.soylent.com/')
+
+indicoio.keywords(page.content)
