082c30faa0161b3f3b42f84cce4ee7b7f499f5d7
KYLE SCHILLER <schillerk@carleton.edu>
2017-04-14 20:20:15 -0500

    printing

--- hack.py
+++ hack.py
@@ -1,9 +1,11 @@
 from lxml import html
 import requests
-import indico
+import indicoio
 
 indicoio.config.api_key = '246290703649a7500961ea78369dbce8'
 
 page = requests.get('https://www.soylent.com/')
+content = page.content
 
-indicoio.keywords(page.content)
+print indicoio.keywords(content)
+print content
